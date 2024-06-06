ace11e948cf4d5d6a06859dcf42b09f60ef27db8
leon332157 <leon332157@gmail.com>
2024-06-06 00:05:34 -0400

    feat: add term and limit

--- script.ts
+++ script.ts
@@ -1,26 +1,69 @@
-import { SpotifyApi, AuthorizationCodeWithPKCEStrategy, SimplifiedPlaylist } from "@spotify/web-api-ts-sdk";
+import { SpotifyApi, AuthorizationCodeWithPKCEStrategy, SimplifiedPlaylist, Track, MaxInt } from "@spotify/web-api-ts-sdk";
 
+// spotify init
 const redirectURL = window.location.origin + "/callback";
-
 const implicitGrantStrategy = new AuthorizationCodeWithPKCEStrategy(
     "2450b38b0c0744948cf541349f2a5d15",
     redirectURL,
-    ['user-top-read', 'playlist-modify-public', 'playlist-read-private', 'playlist-modify-private', 'user-read-private', 'user-read-email']
+    ['user-top-read', 'playlist-modify-public', 'playlist-read-private', 'playlist-modify-private', 'user-read-email']
 );
-
 const spotify = new SpotifyApi(implicitGrantStrategy);
-const profile = await spotify.currentUser.profile();
-const userId = profile.id;
+let userId: string | null = null;
 
-document.getElementById("displayName")!.innerText = profile.display_name;
-const topsongs = await spotify.currentUser.topItems("tracks", "short_term", 50)
+if (await spotify.getAccessToken() === null) {
+    await spotify.authenticate()
+}
 
-const songsList = document.getElementById('songsList');
-songsList!.innerHTML = "";
-topsongs.items.forEach((song) => {
-    const allArtists = song.artists.reduce((acc, artist) => { return acc += `, ${artist.name}` }, '');
-    songsList!.innerHTML += `<li>${allArtists} - ${song.name}</li>`;
+spotify.currentUser.profile().then((profile) => {
+    userId = profile.id;
+    document.getElementById("displayName")!.innerText = `logged in as ${profile.display_name}`;
 })
+
+function updateSongListUI(songs: Track[],name:string) {
+    songsList!.innerHTML = `<p>${name}</p>`;
+    songs.forEach((song) => {
+        const allArtists = song.artists.reduce((acc, artist, idx) => { return (idx > 0 ? acc += `, ${artist.name}` : artist.name) }, '');
+        songsList!.innerHTML += `<li>${allArtists} - ${song.name}</li>`;
+    })
+}
+
+const windowURL = new URL(window.location.href);
+// dom stuff
+const songsList = document.getElementById('songsList');
+const result = document.getElementById('result');
+const plBtn = document.getElementById('createPlaylistBtn');
+
+const limitp = windowURL.searchParams.get("limit")
+let limit: MaxInt<50> = 50;
+
+if (limitp !== null) {
+    if (Number.parseInt(limitp) > 50 || Number.parseInt(limitp) < 1) limit = 50; // invalid limit 
+    else limit = Number.parseInt(limitp) as MaxInt<50>;
+}
+
+let topSongs = await spotify.currentUser.topItems("tracks", "short_term", limit) // default to short term
+plBtn!.onclick = () => createPlaylist(topSongs.items, `My Top ${limit} Songs - Short Term`);
+switch (windowURL.pathname) {
+    case "/callback":
+        window.location.href = window.location.origin;
+    case "/":
+    case "/short":
+        updateSongListUI(topSongs.items,`My Top ${limit} Songs - Short Term`);
+        break;
+    case "/medium":
+        topSongs = await spotify.currentUser.topItems("tracks", "medium_term", limit)
+        plBtn!.onclick = () => createPlaylist(topSongs.items, `My Top ${limit} Songs - Medium Term`);
+        updateSongListUI(topSongs.items,`My Top ${limit} Songs - Medium Term`);
+        break;
+    case "/long":
+        topSongs = await spotify.currentUser.topItems("tracks", "long_term", limit)
+        plBtn!.onclick = () => createPlaylist(topSongs.items, `My Top ${limit} Songs - Long Term`);
+        updateSongListUI(topSongs.items,`My Top ${limit} Songs - Long Term`);
+        break;
+    default:
+        console.log('default', windowURL.pathname);
+}
+
 
 async function getAllUserPlaylists() {
     const total = new Array<SimplifiedPlaylist>();
@@ -34,12 +77,12 @@ async function getAllUserPlaylists() {
     return total;
 }
 
-async function findTopPlaylist() {
+async function existsPlaylist(name: string) {
     let temp = await spotify.currentUser.playlists.playlists();
     let offset = 0;
     while (temp.next != null) {
         offset += 50
-        const findRes = temp.items.find((playlist) => { return playlist.name == "My Top 50 Songs - Short Term" });
+        const findRes = temp.items.find((playlist) => { return playlist.name == name });
         if (findRes !== undefined) {
             return findRes;
         }
@@ -48,20 +91,36 @@ async function findTopPlaylist() {
     return null;
 }
 
-export async function createPlaylist() {
+export async function createPlaylist(songs: Track[], name: string) {
     document.getElementById("result")!.innerHTML = "Creating..."
-    let topPlaylist = await findTopPlaylist();
-    if (topPlaylist === null) {
-        topPlaylist = await spotify.playlists.createPlaylist(userId, { name: "My Top 50 Songs - Short Term", public: false });
-    }
-    if (topPlaylist !== null) {
-        await spotify.playlists.updatePlaylistItems(topPlaylist.id, { uris: topsongs.items.reduce((acc, curr) => { acc.push(curr.uri); return acc; }, new Array<string>()) })
-    }
-    else {
-        console.error("Playlist is undefined");
-        document.getElementById("result")!.innerText = "Failed, check console";
+    try {
+        if (userId === null) throw new Error("userid is null");
+        let topPlaylist = await existsPlaylist(name);
+        if (topPlaylist === null) {
+
+            topPlaylist = await spotify.playlists.createPlaylist(userId, { name: name, public: false });
+        }
+        if (topPlaylist !== null) {
+            await spotify.playlists.updatePlaylistItems(topPlaylist.id, { uris: songs.reduce((acc, curr) => { acc.push(curr.uri); return acc; }, new Array<string>()) })
+        }
+        else {
+            console.error("playlist is undefined");
+            result!.innerText = "error creating playlist: playlist is undefined";
+            return;
+        }
+    } catch (e) {
+        console.error(e);
+        result!.innerText = `error creating playlist: ${e}`;
         return;
     }
-    document.getElementById("result")!.innerText = "Playlist created successfully: My Top 50 Songs - Short Term";
+    result!.innerText = `playlist created successfully: ${name}`;
 }
-document.getElementById("createPlaylistBtn")!.onclick = createPlaylist
+
+
+function logout() {
+    spotify.logOut();
+    localStorage.clear();
+    window.location.reload();
+}
+
+document.getElementById("logoutBtn")!.onclick = logout;
